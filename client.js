32944e1b6c07f4f0c585a25f31843710043031b1
Alfin Akhret <alfin@qasico.com>
2016-03-08 04:34:16 +0700

    add basic CLI scenario

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ window = process;   // simulate window global object on CLI environment ;-p
 // import all modules
 var Watcher = require('./libs/file_watcher');
 var program = require('commander');
-// var cli = require('')
+var prompt = require('prompt');
 
 // CONFIGURATION
 // ===================================================================
@@ -15,26 +15,41 @@ var serverPort = config.server.port;
 // --------------------------------------------------------------------
 var w = new Watcher(__dirname + '/' + config.pubDir);
 var fileList = [];
-w.on('process', function(files) {
-    fileList = files;
-});
 
 // Client socket configuration
 // ---------------------------------------------------------------------
 // Socket io libs
 var io = require('socket.io-client');
-var p2p = require('socket.io-p2p');
-
-var s = io.connect(serverAddr + ':' + serverPort);
-s.emit('new-connection', config.client);
-s.on('client_disconnect', function(data) {
-    console.log('Peer: ' + data.username + ' is disconnected');
-});
-
-// run file watcher
-w.start();
+var p2p = require('socket.io-p2p');  
 
 // MAIN PROGRAM
+// =====================================================================
+program
+    .arguments('<host>')
+    .arguments('<port>')
+    .action(function(host, port) {
+        // create new socket connection
+        if(host && port) 
+            var s = io.connect(host + ':' + port);
+        else
+            var s = io.connect(serverAddr + ':' + serverPort);
+
+        // send new connection event to the server
+        s.emit('new-connection', config.client);
+
+        // handler for:
+        // client_disconnect event 
+        s.on('client_disconnect', function(data) {
+            console.log('Peer: ' + data.username + ' is disconnected');
+        });
+
+        // run file watcher
+        w.start();
+        w.on('process', function(files) {
+            fileList = files;
+        });  
+    })
+    .parse(process.argv);
 
 
 
