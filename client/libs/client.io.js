3681234eab3ce6ba8ae67d60c32a521b1e7a85b4
Alfin Akhret <alfin@qasico.com>
2016-03-08 12:08:40 +0700

    manage to send/retrieve client-server message. still trouble getting the socket.io p2p to work

--- client/libs/client.io.js
+++ client/libs/client.io.js
@@ -1,16 +1,36 @@
 // CONFIGURATION
 // ===================================================================
 var config = require('./config.js');
+var jq = require('jquery');
 var serverAddr = config.server.ipAddress;
 var serverPort = config.server.port;
 
-// Client socket configuration
-// ---------------------------------------------------------------------
-// Socket io libs
-var io = require('socket.io-client');
-var p2p = require('socket.io-p2p');
 
-var s = io.connect('http://localhost:8000');
+jq(document).ready(function() {
+    // Client socket configuration
+    // ---------------------------------------------------------------------
+    // Socket io libs
+    // var Socketiop2p = require('socket.io-p2p');
+    var io = require('socket.io-client');
+    var socket = io.connect('http://localhost:8000');
+    // socket.io.uri = 'http://localhost:8000';
+    // var opts = {peerOpts: {trickle: false}, autoUpgrade: false}
+    // var p2psocket = new Socketiop2p(socket, opts);
+
+    // test
+    jq('#ping').click(function() {
+        socket.emit('ping-test');    
+    });
+
+    socket.on('ping-reply', function(data) {
+        jq('#result').append('<li>').text(data.message);
+    });
+
+    // s.on('ping-reply', function(data) {
+    //     jq('#result').append('<li>').text(data.message);
+    // });
+});
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -3,44 +3,51 @@ var http = require('http').Server();
 var io = require('socket.io')(http);
 var p2pServer = require('socket.io-p2p-server').Server;
 
-io.use(p2pServer);
 http.listen(8000, function() {
     console.log("Server is listening on port 8000");
 });
 
+// io.use(p2pServer);
+
 var clientsData = [];
 
 io.on('connection', function(socket) {
+    console.log('new conn');
 
-    socket.on('new_connection', function(data) {
-        clientsData[socket.id] = data;
+
+    socket.on('ping-test', function() {
+        socket.emit('ping-reply', {message: 'ping reply from server'});
     });
 
-    socket.on('disconnect', function() {
-        socket.broadcast.emit('client_disconnect', clientsData[socket.id]);
-        delete clientsData[socket.id];
-    });
+    // socket.on('new_connection', function(data) {
+    //     clientsData[socket.id] = data;
+    // });
 
-    socket.on('show_peers', function() {
-        var peers = [];
-        for (var index in clientsData) {
-            if(index != socket.id) {
-                peers.push(clientsData[index].username);
-            }
-        }
-        socket.emit('all_peers', peers);
-    });
+    // socket.on('disconnect', function() {
+    //     socket.broadcast.emit('client_disconnect', clientsData[socket.id]);
+    //     delete clientsData[socket.id];
+    // });
 
-    socket.on('updateFileIndex', function(data) {
-        clientsData[socket.id].files = data;
-    });
+    // socket.on('show_peers', function() {
+    //     var peers = [];
+    //     for (var index in clientsData) {
+    //         if(index != socket.id) {
+    //             peers.push(clientsData[index].username);
+    //         }
+    //     }
+    //     socket.emit('all_peers', peers);
+    // });
 
-    socket.on('ls', function(data) {
-        for (var i in clientsData) {
-            if (clientsData[i].username === data) {
-                socket.emit('files_found', clientsData[i].files);
-            }
-        }
-    });
+    // socket.on('updateFileIndex', function(data) {
+    //     clientsData[socket.id].files = data;
+    // });
+
+    // socket.on('ls', function(data) {
+    //     for (var i in clientsData) {
+    //         if (clientsData[i].username === data) {
+    //             socket.emit('files_found', clientsData[i].files);
+    //         }
+    //     }
+    // });
 
 });
